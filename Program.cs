6482e3cee516a79eb1781fdc5bb078cb37c38988
Lukeshaw36 <103990852+Lukeshaw36@users.noreply.github.com>
2023-11-30 08:55:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Palindrome;
+
+  public class palindrome 
+{
+
+
+public static void Main(string[] args)
+    {
+        //STRING  MANIPULATION (PALINDROME)
+        string Firstword = "Samuel";
+        string reversedword = ReverseString(Firstword);
+
+        Console.WriteLine($"First Word: {Firstword}");
+        Console.WriteLine($"Reversed Word: {reversedword}");
+
+
+        bool isPalindrome = IsPalindrome(Firstword);
+
+        Console.WriteLine($"Is Palindrome: {isPalindrome}");
+        //method checking if its a palindrome
+        static bool IsPalindrome(string input)
+        {
+            string reversedString = ReverseString(input);
+            return input.Replace(" ", "").Equals(reversedString.Replace(" ", ""), StringComparison.OrdinalIgnoreCase);
+        }
+
+
+        //method used in coverting to a charArray
+        static string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+    }
+}
